cfa82e68ce70b756347694505066eb5e5441f7c1
kauderk <65237382+kauderk@users.noreply.github.com>
2022-10-03 17:16:48 -0500

    fix: app/docs sveltekit adapter, using netlify
    plus .gitignore duplicate removal
    And "rimraf .netlify && vite build" cleaning before building

--- apps/docs/vite.config.ts
+++ apps/docs/vite.config.ts
@@ -1,18 +1,34 @@
 import { defineConfig } from 'vite';
 import { sveltekit } from '@sveltejs/kit/vite';
-import path from 'path';
+import path, { join } from 'path';
+import { readFileSync } from 'fs';
+import { cwd } from 'process';
+
+// @ts-ignore
+const pkg = JSON.parse(readFileSync(join(cwd(), 'package.json')));
 
 export default defineConfig({
 	plugins: [sveltekit()],
 	resolve: {
 		alias: {
 			$components: path.resolve('./src/components')
-		}
+		},
+		preserveSymlinks: false
+	},
+	build: {
+		target: ['es2020'],
+		sourcemap: true
 	},
 	ssr: {
-		noExternal: ['@popperjs/core', 'dayjs', '@popperjs+core']
+		noExternal: [
+			...Object.keys(pkg.dependencies || {}),
+			'@popperjs/core',
+			'dayjs',
+			'@popperjs+core'
+		]
 	},
 	server: {
+		open: '/',
 		fs: {
 			allow: ['.']
 		}
