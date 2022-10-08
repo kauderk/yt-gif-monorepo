399776842586e4eb05e50f53705277e3e3d3a021
d-ibarra1 <dib.18800k@gmail.com>
2022-10-08 16:33:29 -0500

    docs: range mostrado en @apps/histoire

---
+++ apps/histoire/src/lib/Range.story.svelte
@@ -0,0 +1,9 @@
+<script>
+	export let Hst
+	import Range from '@packages/yt-gif/src/v0.3.0/components/Range/Range.svelte'
+	import { iframe_buffer_slider } from '@packages/yt-gif/src/v0.3.0/App/Miscellaneous.ts'
+</script>
+
+<Hst.Story title="Range">
+	<Range options={iframe_buffer_slider.range} />
+</Hst.Story>
