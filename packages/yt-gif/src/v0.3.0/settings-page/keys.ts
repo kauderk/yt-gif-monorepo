5b5a4aaee573786f7a49b55b47612e7c87a198f3
kauderk <65237382+kauderk@users.noreply.github.com>
2022-10-30 04:31:08 -0500

    chore: branch cleanup

--- packages/yt-gif/src/v0.3.0/settings-page/keys.ts
+++ packages/yt-gif/src/v0.3.0/settings-page/keys.ts
@@ -1,6 +1,6 @@
 import { getPageUidSync, getOrCreatePageUid } from '$lib/utils-roam-alpha-api'
 
-export const TARGET_PAGE = 'roam/js/kauderk/yt-gif/other'
+export const TARGET_PAGE = 'roam/js/kauderk/yt-gif/settings'
 export let TARGET_UID = getPageUidSync(TARGET_PAGE)
 export async function TryToSetTargetUID() {
 	TARGET_UID = await getOrCreatePageUid(TARGET_PAGE)
