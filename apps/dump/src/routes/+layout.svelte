816f5f7095dd43d41480c173bd9d69aff1134001
kauderk <65237382+kauderk@users.noreply.github.com>
2022-10-31 07:36:38 -0500

    feat: added nav loader on navigation

---
+++ apps/dump/src/routes/+layout.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+	import '../app.css'
+
+	import { navigating } from '$app/stores'
+	import navState from '$cmp/UX/loader/store'
+	import NavLoader from '$cmp/UX/loader/index.svelte'
+
+	// if we're navigating, set the store accordingly
+	$: $navState = $navigating != null ? 'loading' : 'loaded'
+</script>
+
+<NavLoader />
+
+<slot />
