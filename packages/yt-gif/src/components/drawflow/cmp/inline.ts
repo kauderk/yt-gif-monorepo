ab0a64a16b95be6f3dcc3f21d16231fa8ed7b077
kauderk <65237382+kauderk@users.noreply.github.com>
2022-11-07 16:07:10 -0500

    feat: added Shadow Steam Store block

--- packages/yt-gif/src/components/drawflow/cmp/inline.ts
+++ packages/yt-gif/src/components/drawflow/cmp/inline.ts
@@ -3,39 +3,51 @@ import type Drawflow from '$cmp/drawflow/src/drawflow'
 import Content from './Content.svelte'
 import Simple from './blocks/Simple.svelte'
 import Player from './blocks/Player.svelte'
+import ShadowBlock from './blocks/Shadow.svelte'
 
 export function createNodeComponents(editor: Drawflow) {
 	editor.addNode(
-		'github',
+		'graph-node',
 		1,
 		1,
 		600,
 		210,
-		'github',
+		'graph-node',
 		{},
 		'SvelteContent', // drawflow/cmp/Content.svelte
 		'svelte'
 	)
 	editor.addNode(
-		'simple',
+		'graph-node',
 		1,
 		1,
 		863,
 		515,
-		'simple',
+		'graph-node',
 		{},
 		'SimpleBlock', // drawflow/cmp/Simple.svelte
 		'svelte'
 	)
 	editor.addNode(
-		'simple',
+		'graph-node',
 		1,
 		1,
 		1000,
 		715,
-		'simple',
+		'graph-node',
 		{},
 		'PlayerBlock', // drawflow/cmp/Simple.svelte
+		'svelte'
+	)
+	editor.addNode(
+		'graph-node',
+		1,
+		1,
+		1000,
+		315,
+		'graph-node',
+		{},
+		'ShadowBlock', // drawflow/cmp/ShadowBlock.svelte
 		'svelte'
 	)
 }
@@ -43,4 +55,5 @@ export function registerNodeComponents(editor: Drawflow) {
 	editor.registerNode('SvelteContent', Content)
 	editor.registerNode('SimpleBlock', Simple)
 	editor.registerNode('PlayerBlock', Player)
+	editor.registerNode('ShadowBlock', ShadowBlock)
 }
