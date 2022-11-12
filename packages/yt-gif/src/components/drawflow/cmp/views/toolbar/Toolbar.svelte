e5924a8a6814776ee03911491b3d5b655f7e10a6
kauderk <65237382+kauderk@users.noreply.github.com>
2022-11-12 09:38:15 -0500

    feat: toolbar tooltips

--- packages/yt-gif/src/components/drawflow/cmp/views/toolbar/Toolbar.svelte
+++ packages/yt-gif/src/components/drawflow/cmp/views/toolbar/Toolbar.svelte
@@ -5,39 +5,82 @@
 	import Button from './Button.svelte'
 	import { register, startTour } from './Tour.svelte'
 	import EmailFeedback from './EmailFeedback.svelte'
+	import Tooltip from './Tooltip.svelte'
+	import { dev } from '$app/environment'
 
 	onMount(() => state.useLocalStorage())
+	let showI = false
 </script>
 
 <svelte:component this={TopLeft}>
 	<div id="controls">
-		<label for="none">View</label>
-
-		<div use:register={'You can use filters too.'}>
-			<Button active="fullGraph" on="diagram-project" />
-		</div>
-		<div use:register={'You can sort customers here.'}>
-			<Button active="leftSidebar" on="sidebar" />
-		</div>
 		<div
-			use:register={'Country filter is already populated with the existing countries from our customers database'}>
-			<Button active="rightSidebar" on="sidebar-flip" />
-		</div>
-		<div use:register={'F'}>
-			<Button active="Layouts" on="gallery-thumbnails" />
+			class="buttons"
+			on:mouseenter={() => (showI = true)}
+			on:mouseleave={() => (showI = false)}>
+			<label for="none" class="indicator">
+				View
+				{#if showI}
+					<Tooltip info={['Show me around']}>
+						<span
+							class="indicator-item badge"
+							style="color: #8BC34A">
+							<button
+								on:click={() =>
+									startTour({
+										message:
+											"Welcome, let's start the tour now.",
+										step: 0,
+									})}>
+								<i class="fa-sharp fa-solid fa-circle-info" />
+							</button>
+						</span>
+					</Tooltip>
+				{/if}
+			</label>
+			<div use:register={'You can use filters too.'}>
+				<Button active="fullGraph" on="diagram-project" />
+			</div>
+			<div use:register={'You can sort customers here.'}>
+				<Button active="leftSidebar" on="sidebar" />
+			</div>
+			<div
+				use:register={'Country filter is already populated with the existing countries from our customers database'}>
+				<Button active="rightSidebar" on="sidebar-flip" />
+			</div>
+			<div use:register={'F'}>
+				<Button active="Layouts" on="gallery-thumbnails" />
+			</div>
 		</div>
 
-		<button
-			on:click={() =>
-				startTour({
-					message: "Welcome, let's start the tour now.",
-					step: 0,
-				})}>go</button>
-		<EmailFeedback />
+		<div class="feedback">
+			<i class="fa-solid fa-message-pen" />
+			<Tooltip
+				info={dev
+					? [
+							'This is a development evnrimomment',
+							`AVOID sending "SCREENSHOTS"`,
+							'COORS is disabled',
+					  ]
+					: [
+							`🕵️ It's completely anonymous`,
+							`🛡️ Help use improve the user experience`,
+							'',
+					  ]}>
+				<EmailFeedback />
+			</Tooltip>
+		</div>
 	</div>
 </svelte:component>
 
 <style lang="scss">
+	.feedback,
+	.buttons {
+		display: flex;
+		flex-direction: row;
+		align-items: center;
+		gap: 0.5em;
+	}
 	#controls {
 		background: rgba(50, 50, 50, 0.2);
 		backdrop-filter: blur(3px);
@@ -46,7 +89,7 @@
 		display: flex;
 		align-items: center;
 		justify-content: space-around;
-		gap: 0.5rem;
+		gap: 1rem;
 		padding: 0.1em 0.5em;
 	}
 
