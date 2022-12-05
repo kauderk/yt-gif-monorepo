7885a286eb7179d6ab36998189f3f164d1ec7c32
kauderk <65237382+kauderk@users.noreply.github.com>
2022-12-05 17:55:23 -0500

    tweak: added basic counter component

--- packages/yt-gif/src/components/text-editor/Editor.svelte
+++ packages/yt-gif/src/components/text-editor/Editor.svelte
@@ -1,15 +1,20 @@
 <script lang="ts">
-	import { onMount, onDestroy } from 'svelte'
+	import { onMount } from 'svelte'
 	import type { Readable } from 'svelte/store'
 	import { createEditor, EditorContent, type Editor } from 'svelte-tiptap'
 	import StarterKit from '@tiptap/starter-kit'
+	import { SvelteCounterExtension } from './tiptap/extension/counter'
 
 	let editor: Readable<Editor>
 
 	onMount(() => {
 		editor = createEditor({
-			extensions: [StarterKit],
-			content: `Hello world! sdlkfjsdlkfjksld fjkl`,
+			extensions: [StarterKit, SvelteCounterExtension],
+			content: `
+        <p>This is still the text editor you’re used to, but enriched with node views.</p>
+        <svelte-counter-component count="0"></svelte-counter-component>
+        <p>Did you see that? That’s a Svelte component. We are really living in the future.</p>
+      `,
 		})
 	})
 </script>
---
+++ packages/yt-gif/src/components/text-editor/tiptap/extension/Counter.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+	import type { NodeViewProps } from '@tiptap/core'
+	import cx from 'classnames'
+	import { NodeViewWrapper } from 'svelte-tiptap'
+
+	export let node: NodeViewProps['node']
+	export let updateAttributes: NodeViewProps['updateAttributes']
+	export let selected: NodeViewProps['selected'] = false
+
+	const handleClick = () => {
+		updateAttributes({ count: node.attrs.count + 1 })
+	}
+</script>
+
+<NodeViewWrapper class={cx('svelte-component', { selected })}>
+	<span class="label">Svelte Component</span>
+
+	<div class="content">
+		<button on:click={handleClick} type="button">
+			This button has been clicked {node.attrs.count} times.
+		</button>
+	</div>
+</NodeViewWrapper>
